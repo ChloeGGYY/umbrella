295d11619237c2786f2ec4e1166822969bb3eee7
ChloeGGYY <97067927+ChloeGGYY@users.noreply.github.com>
2024-04-07 22:13:07 +0000

    umbrella-next hour

--- umbrella.rb
+++ umbrella.rb
@@ -1,27 +1,25 @@
 require "http"
 require "json"
 
+#GMAPS API -------------------------------------------------------------
+
 pp "Where are you located?"
-user_location = gets.chomp
-#user_location = "Chicago"
+#user_location = gets.chomp
+
+user_location = "Chicago"
 pp user_location
 
 maps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + user_location + "&key=" + ENV.fetch("GMAPS_KEY")
 
-resp = HTTP.get(maps_url)
-
-raw_response = resp.to_s
+response = HTTP.get(maps_url)
+raw_response = response.to_s
 
 parsed_response = JSON.parse(raw_response)
-
 results =  parsed_response.fetch("results")
 
 first_result = results[0]
-
 geo = first_result.fetch("geometry")
-
 location = geo.fetch("location")
-
 lat = location.fetch("lat")
 lng = location.fetch("lng")
 
@@ -29,22 +27,31 @@ pp lat
 pp lng
 
 
-=begin
-# Hidden variables
-pirate_weather_api_key = ENV["PIRATE_WEATHER_KEY"]
-
-# Assemble the full URL string by adding the first part, the API token, and the last part together
-pirate_weather_url = "https://api.pirateweather.net/forecast/" + pirate_weather_api_key + "/41.8887,-87.6355"
-
-# Place a GET request to the URL
-raw_response = HTTP.get(pirate_weather_url)
+#WEATHER API -------------------------------------------------------------
 
 
-parsed_response = JSON.parse(raw_response)
+weather_url = "https://api.pirateweather.net/forecast/" + ENV.fetch("PIRATE_WEATHER_KEY") + "/" + lat.to_s + "," + lng.to_s
 
-currently_hash = parsed_response.fetch("currently")
+pp weather_url 
+response2 = HTTP.get(weather_url)
+raw_response2 = response2.to_s
 
-current_temp = currently_hash.fetch("temperature")
+parsed_response2 = JSON.parse(raw_response2)
 
-puts "The current temperature is " + current_temp.to_s + "."
-=end
+current = parsed_response2.fetch("currently")
+current_temp = current.fetch("temperature")
+
+puts "It is currently #{current_temp}°F."
+
+#NEXT HOUR -------------------------------------------------
+minutely_hash = parsed_response2.fetch("minutely", false)
+
+if minutely_hash
+  next_hour_summary = minutely_hash.fetch("summary")
+
+  puts "Next hour: #{next_hour_summary}"
+end
+
+# PRECIPITATION -------------------------------------------------
+
+
