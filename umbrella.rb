b9f03b38a8a2a0fdcf2aee2f707d7424dec4d8d9
ChloeGGYY <97067927+ChloeGGYY@users.noreply.github.com>
2024-04-19 15:10:10 +0000

    umbrella (with my stuff)

--- umbrella.rb
+++ umbrella.rb
@@ -1,6 +1,47 @@
 require "http"
 require "json"
 
+=begin
+
+# Single string of addresses separated by underscores
+address_string = "Nova Brasília_Avenida Itaoca_Rua Nova Brasília_Ramos_Bonsucesso_Praça do Terco_São Paulo_Catacumba_Buraco do Sapo_Itaoca_Avenida Nova Brasília_Alvorada_Inferno Verde_Fazendinha_Barra de Tijuca_Complexo de Alemão_Morro de Alemão_Rocinha_Jacarezinho_Copacabana_Grota_Praça_Rio Grande do Norte_Niteroi_Campo Grande_Sao Gonçalo_Niteroi_Natal_Gloria_Irajá_Japeri_Guadalupe_Estacio de Sá_Niteroi_Santa Cruz_Praça do Terco_João Pessoa_Paraiba_Natal_Rio Grande do Norte"
+
+# Split the string into an array of addresses
+addresses = address_string.split("_")
+
+# Google Maps API key
+api_key = ENV.fetch("GMAPS_KEY")
+
+# Iterate over each address
+addresses.each do |address|
+  maps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{address}&key=#{api_key}"
+  
+  # Send the request to the Google Maps API
+  response = HTTP.get(maps_url)
+  raw_response = response.to_s
+  
+  # Parse the JSON response
+  parsed_response = JSON.parse(raw_response)
+  results = parsed_response.fetch("results")
+  
+  # Check if results were found
+  if results.any?
+    first_result = results[0]
+    geo = first_result.fetch("geometry")
+    location = geo.fetch("location")
+    lat = location.fetch("lat")
+    lng = location.fetch("lng")
+    
+    # Print the original address and its coordinates
+    puts "#{address} (#{lat}, #{lng})"
+  else
+    puts "No results found for #{address}"
+  end
+end
+
+=end
+
+
 #GMAPS API -------------------------------------------------------------
 
 pp "Where are you located?"
@@ -11,8 +52,13 @@ pp user_location
 
 maps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + user_location + "&key=" + ENV.fetch("GMAPS_KEY")
 
+pp maps_url
+require "http"
 response = HTTP.get(maps_url)
 raw_response = response.to_s
+
+
+# pp raw_response
 
 parsed_response = JSON.parse(raw_response)
 results =  parsed_response.fetch("results")
@@ -25,7 +71,6 @@ lng = location.fetch("lng")
 
 pp lat
 pp lng
-
 
 #WEATHER API -------------------------------------------------------------
 
@@ -54,4 +99,5 @@ end
 
 # PRECIPITATION -------------------------------------------------
 
-
+hourly_hash = parsed_response2.fetch("hourly")
+ pp hourly_hash.class
